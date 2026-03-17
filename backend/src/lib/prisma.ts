dc252affd93c2d108aa73eba2aeb22b6574a8207
Daniel Ogbuike <ogbuikedaniel@gmail.com>
2026-03-17 22:25:21 +0100

    fix prisma v7 issues

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,3 +1,8 @@
-import { PrismaClient } from "@prisma/client/extension";
-
-export const prisma = new PrismaClient();
+import PG from "pg";
+import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+const pool = new PG.Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool as any);
+export const prisma = new PrismaClient({
+  adapter,
+});
