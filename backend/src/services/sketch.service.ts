70a3a5ba06d9c426146b301fce3dfa6d38941a33
Daniel Ogbuike <ogbuikedaniel@gmail.com>
2026-03-17 21:32:55 +0100

    setup sketch service

---
+++ backend/src/services/sketch.service.ts
@@ -0,0 +1,22 @@
+import type { Sketch } from "../generated/prisma/client.js";
+import { prisma } from "../lib/prisma.js";
+import type { SketchInput } from "../utils/validation.js";
+
+export class SketchService {
+  async saveSketch({ name, area, geojson }: SketchInput) {
+    const geojsonStr =
+      typeof geojson === "string" ? geojson : JSON.stringify(geojson);
+    const result = await prisma.$queryRaw`
+    INSERT INTO "Sketch" (id, name, area, "createdAt", geom) VALUES (
+        gen_random_uuid(),
+        ${name},
+        ${area},
+        now(),
+        ST_SetSRID(ST_GeomFromGeoJSON(${geojsonStr}), 4326)
+    )
+    RETURNING id, name, area, "createdAt";
+    `;
+
+    return result;
+  }
+}
