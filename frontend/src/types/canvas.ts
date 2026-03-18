a4ec3a7517de6c605da071f5805adcb62c46587c
Daniel Ogbuike <ogbuikedaniel@gmail.com>
2026-03-18 19:19:57 +0100

    canvas types

---
+++ frontend/src/types/canvas.ts
@@ -0,0 +1,22 @@
+export interface Point {
+  x: number;
+  y: number;
+}
+export interface BaseShape {
+  id: string;
+}
+export interface PolygonShape extends BaseShape {
+  type: "polygon";
+  points: Point[];
+  area: number;
+  geojson: unknown;
+}
+export interface FrameShape extends BaseShape {
+  type: "frame";
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+export type Shape = PolygonShape | FrameShape;
+export type Tool = "select" | "polygon" | "frame";
