747376139d25d72a003a7d175256918f52b7f39d
Daniel Ogbuike <ogbuikedaniel@gmail.com>
2026-04-21 22:16:10 +0100

    pwa enhancements

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ohv1s5nh0i"
+    "revision": "0.tpvl8q4pri"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -21,16 +21,14 @@ export default defineConfig({
         display: "standalone",
         icons: [
           {
-            src: "/icon.svg",
+            src: "/pwa-192x192.png",
             sizes: "192x192",
-            type: "image/svg+xml",
-            purpose: "any maskable"
+            type: "image/png"
           },
           {
-            src: "/icon.svg",
+            src: "/pwa-512x512.png",
             sizes: "512x512",
-            type: "image/svg+xml",
-            purpose: "any maskable"
+            type: "image/png"
           }
         ],
       },
